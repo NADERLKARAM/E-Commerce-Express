c8e549c7de721a10a036708a47a6f891fb81ef0b
nader <naedrkamal4@gmail.com>
2024-01-08 22:08:17 +0300

    solve path link problem

--- services/subCategoryService.js
+++ services/subCategoryService.js
@@ -1,5 +1,6 @@
-const factory = require('./handlersFactory');
 const SubCategory = require('../models/subCategoryModel');
+const factory = require('./handlersFactory');
+
 
 exports.setCategoryIdToBody = (req, res, next) => {
   // Nested route (Create)
@@ -8,7 +9,7 @@ exports.setCategoryIdToBody = (req, res, next) => {
 };
 
 // Nested route
-// GET /categories/:categoryId/subcategories
+// GET /api/v1/categories/:categoryId/subcategories
 exports.createFilterObj = (req, res, next) => {
   let filterObject = {};
   if (req.params.categoryId) filterObject = { category: req.params.categoryId };
@@ -17,27 +18,26 @@ exports.createFilterObj = (req, res, next) => {
 };
 
 // @desc    Get list of subcategories
-// @route   GET /subcategories
+// @route   GET /api/v1/subcategories
 // @access  Public
 exports.getSubCategories = factory.getAll(SubCategory);
 
 // @desc    Get specific subcategory by id
-// @route   GET /subcategories/:id
+// @route   GET /api/v1/subcategories/:id
 // @access  Public
 exports.getSubCategory = factory.getOne(SubCategory);
 
 // @desc    Create subCategory
-// @route   POST  /subcategories
+// @route   POST  /api/v1/subcategories
 // @access  Private
 exports.createSubCategory = factory.createOne(SubCategory);
 
 // @desc    Update specific subcategory
-// @route   PUT /subcategories/:id
+// @route   PUT /api/v1/subcategories/:id
 // @access  Private
 exports.updateSubCategory = factory.updateOne(SubCategory);
 
 // @desc    Delete specific subCategory
-// @route   DELETE /subcategories/:id
+// @route   DELETE /api/v1/subcategories/:id
 // @access  Private
-exports.deleteSubCategory = factory.deleteOne(SubCategory);
 exports.deleteSubCategory = factory.deleteOne(SubCategory);
